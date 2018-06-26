c8a747fcce7dea1c3ee3da918076ccf9b1b2923f
Tong Bao <bao.tt@tengroup.com.vn>
2018-06-26 17:50:53 +0700

    Intro Slider & Search Header

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import Splash from './Component/Login/Splash'
+import IntroSlider from './Component/Login/IntroSlider'
 
-AppRegistry.registerComponent('NguyenKimApp', () => Splash);
+AppRegistry.registerComponent('NguyenKimApp', () => IntroSlider);
